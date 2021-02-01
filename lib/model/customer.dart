a11126a0aeea420ef48a129cbb9aad41046a65d8
unknown <sunilhari496@gmail.com>
2021-02-01 11:23:09 +0530

    first

---
+++ lib/model/customer.dart
@@ -0,0 +1,14 @@
+class CustomerModel {
+  String email;
+  String firstName;
+  String lastname;
+  String password;
+
+  CustomerModel({
+    this.email,
+    this.firstName,
+    this.lastname,
+    this.lastname,
+    this.lastname,
+  });
+}
